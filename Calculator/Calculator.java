4b7a2ff3a99f95e27a747b499deb6ddf62cbf3a8
dashashamis1990 <136595184+dashashamis1990@users.noreply.github.com>
2024-09-15 20:06:43 +0300

    add logger

--- Calculator/Calculator.java
+++ Calculator/Calculator.java
@@ -1,44 +1,46 @@
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class Calculator {
+    private static final Logger logger = Logger.getLogger(Calculator.class.getName());
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         double num1, num2, result;
         char operator;
-        
-        System.out.print("Enter the first number: ");
+
+        logger.info("Enter the first number: ");
         num1 = input.nextDouble();
-        
-        System.out.print("Enter the second number: ");
+
+        logger.info("Enter the second number: ");
         num2 = input.nextDouble();
-        
-        System.out.print("Enter an operator (+, -, *, /): ");
+
+        logger.info("Enter an operator (+, -, *, /): ");
         operator = input.next().charAt(0);
-        
+
         switch (operator) {
             case '+':
                 result = num1 + num2;
-                System.out.println(num1 + " + " + num2 + " = " + result);
+                logger.info(num1 + " + " + num2 + " = " + result);
                 break;
             case '-':
                 result = num1 - num2;
-                System.out.println(num1 + " - " + num2 + " = " + result);
+                logger.info(num1 + " - " + num2 + " = " + result);
                 break;
             case '*':
                 result = num1 * num2;
-                System.out.println(num1 + " * " + num2 + " = " + result);
+                logger.info(num1 + " * " + num2 + " = " + result);
                 break;
             case '/':
                 if (num2 == 0) {
-                    System.out.println("Error: Division by zero");
-                } 
-                else {
+                    logger.info("Error: Division by zero");
+                } else {
                     result = num1 / num2;
-                    System.out.println(num1 + " / " + num2 + " = " + result);
+                    logger.info(num1 + " / " + num2 + " = " + result);
                 }
                 break;
             default:
-                System.out.println("Invalid operator");
+                logger.info("Invalid operator");
                 break;
         }
     }
